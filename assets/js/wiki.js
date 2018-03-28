3683d576ea3fd0d0589bcb7c24da32745d77bb46
amoore16 <amoore16@gmail.com>
2018-03-28 13:22:32 -0600

    Style, list output, and links

--- assets/js/wiki.js
+++ assets/js/wiki.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 
     $("input[type='text']").keypress(function(event){
-        if(event.which === 13){
-            $searchText = $(this).val();
-            $(this).val("");
-            $("li").remove();
+        $searchText = $(this).val();
+         // $(this).val("");
+         
+        if($searchText.length > 0){
             searchWiki($searchText);
+            $("li").remove();   
         }
     });
     function searchWiki(search){
@@ -29,10 +30,12 @@ $(document).ready(function(){
             function(json){
                 var query = json.query.pages;
                 console.log(query);
-
                 for(var i = 0; i < query.length; i++){
-                    $("ul").append("<li>" + query[i].title + "</li>");
+                    $("#content > ul").append("<a href=https://en.wikipedia.org/wiki/"+ query[i].title.replace(/ /g, "_") +" target='_blank'><li>" + query[i].title + "</li></a>");
                 }
+                // for(var i = 0; i < query.length; i++){
+                //     $("ul").append("<li>" + query[i].title + "</li>");
+                // }
             }
         );
     }
