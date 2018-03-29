f06f5e5c796bbe177b201a43d489a43d934ac541
alex moore <amoore16@gmail.com>
2018-03-29 14:52:23 -0600

    added dropdown, redoing CSS

--- assets/js/wiki.js
+++ assets/js/wiki.js
@@ -1,15 +1,56 @@
 $(document).ready(function(){
     
-    $("input[type='text']").keypress(function(event){
-        $searchText = $(this).val();
-        if($searchText.length > 0){
-            searchWiki($searchText);
-            $("li").remove();
+    $("#title").autocomplete({
+        source: function(request, response) {
+            var wikiAPI = "https://en.wikipedia.org/w/api.php";
+            $.getJSON(wikiAPI, 
+                {
+                    "action": "query",
+                    "format": "json",
+                    "origin": "*",
+                    "prop": "pageimages|pageterms",
+                    "generator": "prefixsearch",
+                    "redirects": 1,
+                    "formatversion": "2",
+                    "piprop": "thumbnail",
+                    "pithumbsize": "50",
+                    "pilimit": "10",
+                    "wbptterms": "description",
+                    "gpssearch": request.term,
+                    "gpslimit": "10"
+                },
+                function(json){
+                    var query = json.query.pages;
+                    var srchArr = [];
+                        for(var i = 0; i < query.length; i++){
+                            srchArr.push(query[i].title);
+                        }
+                    response(srchArr);
+                }
+            )
         }
     });
-    function searchWiki(search){
+    
+    $("#title").keypress(function(event){
+        if(event.which === 13){
+            var $searchText = $(this).val();
+            $(this).val("");
+            $("#content > ul").empty();
+            apiSearch($searchText);
+        }
+    });
+    $("#searchBtn").on('click', function(){
+        $searchText = $("#title").val();
+        console.log($searchText);
+        $("#title").val("");
+        $("#content > ul").empty();
+        apiSearch($searchText);
+    });
+
+
+    function apiSearch(search){
         var wikiAPI = "https://en.wikipedia.org/w/api.php";
-        $.getJSON(wikiAPI, 
+        $.getJSON(wikiAPI,
             {
                 "action": "query",
                 "format": "json",
@@ -23,16 +64,14 @@ $(document).ready(function(){
                 "pilimit": "10",
                 "wbptterms": "description",
                 "gpssearch": search,
-                "gpslimit": "10"
+                "gpslimit": "10"  
             },
-            function(json){
+            function (json) {
                 var query = json.query.pages;
-                console.log(json);
-                for(var i = 0; i < query.length; i++){
-                    $("#content > ul").append("<a href=https://en.wikipedia.org/wiki/"+ query[i].title.replace(/ /g, "_") +" target='_blank'><li><h4>" + query[i].title + "</h4><p>" + query[i].terms.description + "</p></li></a>");
-                    //add conditional for displaying description 
+                for (var i = 0; i < query.length; i++){
+                    $("#content > ul").append("<a href='https://en.wikipedia.org/wiki/"+ query[i].title.replace(/ /g,'_') + "' target='_blank'><li>" + query[i].title + "</li></a>")
                 }
             }
-        );
+        )
     }
 });
