bf3b70f3fa2396e1a9bbb5fb829d1306ea104b81
alex moore <amoore16@gmail.com>
2018-03-28 18:14:05 -0600

    little fixes

--- assets/js/wiki.js
+++ assets/js/wiki.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
     
     $("input[type='text']").keypress(function(event){
         $searchText = $(this).val();
-         // $(this).val("");
-         
         if($searchText.length > 0){
             searchWiki($searchText);
             $("li").remove();
@@ -35,9 +33,6 @@ $(document).ready(function(){
                     $("#content > ul").append("<a href=https://en.wikipedia.org/wiki/"+ query[i].title.replace(/ /g, "_") +" target='_blank'><li><h4>" + query[i].title + "</h4><p>" + query[i].terms.description + "</p></li></a>");
                     
                 }
-                // for(var i = 0; i < query.length; i++){
-                //     $("ul").append("<li>" + query[i].title + "</li>");
-                // }
             }
         );
     }
